c145f526f2bd15b190880988b3f31230a795dd4f
Marssssssss <Marssssssss@users.noreply.github.com>
2026-09-25 08:09:22 +0800

    auto: 2026-09-25 08:09 - 726 修正 Go 镜像冗余写法并补 README(访问者双分派)

--- 12-项目工程/02-设计模式与惯用法/访问者双分派与表达式问题/go/main.go
+++ 12-项目工程/02-设计模式与惯用法/访问者双分派与表达式问题/go/main.go
@@ -12,7 +12,8 @@ type Node interface{ Accept(Visitor) int }
 
 type Num struct{ Value int }
 
-func (n *Num) Accept(v Visitor) int { return v.VisitNum(n) } // 双分派:元素替访问者选方法
+// 双分派:元素替访问者选方法。
+func (n *Num) Accept(v Visitor) int { return v.VisitNum(n) }
 
 type Add struct{ L, R Node }
 
@@ -21,16 +22,24 @@ func (a *Add) Accept(v Visitor) int { return v.VisitAdd(a) }
 type EvalVisitor struct{}
 
 func (EvalVisitor) VisitNum(n *Num) int { return n.Value }
-func (EvalVisitor) VisitAdd(a *Add) int { return a.L.Accept(a.v()) + a.R.Accept(a.v()) }
-func (a *Add) v() EvalVisitor           { return EvalVisitor{} }
+func (EvalVisitor) VisitAdd(a *Add) int { return a.L.Accept(a.rv) + a.R.Accept(a.rv) }
+
+// Go 无方法内引用接收者的捷径,这里以字段承载同一访问者实例(示意)。
+type Add2 struct {
+	L, R Node
+	rv   EvalVisitor
+}
 
 type DoubleVisitor struct{}
 
-func (DoubleVisitor) VisitNum(n *Num) int { return n.Value * 2 } // 加操作=新访问者
-func (DoubleVisitor) VisitAdd(a *Add) int { return a.L.Accept(DoubleVisitor{}) + a.R.Accept(DoubleVisitor{}) }
+// 加操作 = 新访问者,元素层级零改动。
+func (DoubleVisitor) VisitNum(n *Num) int { return n.Value * 2 }
+func (DoubleVisitor) VisitAdd(a *Add) int {
+	return a.L.Accept(DoubleVisitor{}) + a.R.Accept(DoubleVisitor{})
+}
 
 func main() {
 	expr := &Add{&Num{1}, &Num{2}}
 	fmt.Println(expr.Accept(EvalVisitor{}))   // 3
-	fmt.Println(expr.Accept(DoubleVisitor{})) // 6,元素层级零改动
+	fmt.Println(expr.Accept(DoubleVisitor{})) // 6
 }
